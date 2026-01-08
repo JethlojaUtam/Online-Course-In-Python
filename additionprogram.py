489084fd8f9bfd9917322ebe183bbc3fd2e0efde
Jethloja Utam <utamjethloja@gmail.com>
2026-01-08 19:04:44 +0530

    Add files via upload

---
+++ additionprogram.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a first number:"))
+y = int(input("Enter a second number:"))
+z = x + y
+print("Addition is:",z)
+
+x = int(input("Enter a first number:"))
+y = int(input("Enter a second number:"))
+print("Addition is:",x + y)
