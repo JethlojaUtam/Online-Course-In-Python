b207066862cc935907683c285ccca4bf298af833
Jethloja Utam <utamjethloja@gmail.com>
2026-01-14 18:28:50 +0530

    Add files via upload

---
+++ datatype1.py
@@ -0,0 +1,19 @@
+x = "123"
+print(x)
+print(type(x))
+
+a = "123"
+print(a)
+print(type(a))
+
+b = int(a)
+print(b)
+print(type(b))
+
+c = float(a)
+print(c)
+print(type(c))
+
+d = complex(a)
+print(d)
+print(type(d))
