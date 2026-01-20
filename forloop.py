43304f910d3286294b81d985c1b12117a4a145c0
Jethloja Utam <utamjethloja@gmail.com>
2026-01-20 12:57:08 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,25 @@
+x = "utam"
+for i in x:
+	print('Hello')
+
+x = "Darshil"
+for i in x:
+	print('Hello')
+	print('Bye')
+
+x = "Darshil"
+for i in x:
+	print('Hello')
+print('Bye')
+
+x = [1,2,3,4,5,6,7,8,9]
+for i in x:
+	print('Hello')
+
+x = [11,56,79,2,45,2,6,7]
+for i in 5:
+	print('Hello')	#Error Type
+
+x = [11,56,79,2,45,2,6,7]
+for i in x:
+	print('Hello')
