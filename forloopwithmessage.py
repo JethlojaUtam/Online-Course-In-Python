9499983047fa7c07e421b1f19a070b63b5a156a1
Jethloja Utam <utamjethloja@gmail.com>
2026-01-21 15:31:14 +0530

    Add files via upload

---
+++ forloopwithmessage.py
@@ -0,0 +1,15 @@
+x = [1,2,3,4,5,6,7,8,9,10]
+for i in x:
+	print(i)
+
+x = [1,2,3,4,5,6,7,8,9,10]
+for i in x:
+	print(i,end="")
+
+y = 'Utampatel'
+for j in y:
+	print(j)
+
+y = 'Utampatel'
+for j in y:
+	print(j,end="")
---
+++ forloopwithrange.py
@@ -0,0 +1,26 @@
+x = range(10)
+for i in x:
+	print(i)
+
+x = range(22,25)
+for i in x:
+	print(i)
+
+for i in range(25,22,-1):
+	print(i)
+
+x = range(50,44,-2)
+for i in x:
+	print(i)
+
+x = range(1,20,1)
+for i in x:
+	print(i)
+
+x = range(1,20,2)
+for i in x:
+	print(i)
+
+x = range(0,20,2)
+for i in x:
+	print(i)
