04ee524d70ad12a300620dd73c05525ee5e5eacc
Jethloja Utam <utamjethloja@gmail.com>
2026-01-22 11:09:44 +0530

    Add files via upload

---
+++ ifconditional.py
@@ -0,0 +1,21 @@
+x = 5
+if x > 0:
+	print("Yes, x is positive")
+	print("Bye Bye!")
+print("Hello")
+
+x = 5
+if x < 0:
+	print("Yes, x is positive")
+	print("Bye Bye!")
+print("Hello")
+
+x = -5
+if x > 0:
+	print("Yes, x is positive")
+	print("Hello")
+print("Bye Bye!")
+
+x = 5
+if x > 0:
+	print(x,"is positive")
