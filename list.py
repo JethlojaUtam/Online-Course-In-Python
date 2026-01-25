7090a7f65167a12336cbca60b0085e619f0f610b
Jethloja Utam <utamjethloja@gmail.com>
2026-01-25 18:21:06 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,7 @@
+l1 = [10,20,30,40,50]
+print(l1)
+print(type(l1))
+
+l2 = [10,3.1,"list",6+7j]
+print(l2)
+print(type(l2))
---
+++ mathfunction.py
@@ -0,0 +1,14 @@
+# Aa badhu kam IDLE ma karvanu rese...
+# Tame IDLE ma jai ne lakhi math. karso ne tiya tenu list aavi jase...
+
+import math
+x = math.factorial(4)
+print(x)
+
+import math
+y = math.lcm(4,6)
+print(y)
+
+import math
+z = math.gcd(4,6)
+print(z)
