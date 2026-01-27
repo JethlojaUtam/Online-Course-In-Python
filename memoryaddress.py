f25eac62379473c355b958efa18e3850cc411987
Jethloja Utam <utamjethloja@gmail.com>
2026-01-27 10:46:15 +0530

    Add files via upload

---
+++ memoryaddress.py
@@ -0,0 +1,15 @@
+x = 5
+print(id(x))
+
+y = 10
+print(id(y))
+
+z = 10
+print(id(z))
+
+w = 5.0
+print(id(w))
+
+print(x == w)
+
+print(x == y)
---
+++ minimumnumberprogram.py
@@ -0,0 +1,26 @@
+p = 22
+q = 25
+if p < q:
+	print(p,"is miniimum number")
+elif q < p:
+	print(q,"is minimum number")
+else:
+	print("Both are equal number")
+
+p = 25
+q = 22
+if p < q:
+	print(p,"is miniimum number")
+elif q < p:
+	print(q,"is minimum number")
+else:
+	print("Both are equal number")
+
+p = 22
+q = 22
+if p < q:
+	print(p,"is minimum number")
+elif q < p:
+	print(q,"is minimum number")
+else:
+	print("Both are equal number")
