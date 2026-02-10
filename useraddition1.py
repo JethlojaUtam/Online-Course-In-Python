2aea3960fbfc0091f962e0d5362dd7e89bc0ef33
Jethloja Utam <utamjethloja@gmail.com>
2026-02-10 11:24:30 +0530

    Add files via upload

---
+++ useraddition1.py
@@ -0,0 +1,12 @@
+x = input("Enter a first number:")
+a = int(x)
+y = input("Enter a second number:")
+b = int(y)
+c = a + b
+print(c)
+
+x = input("Enter a first number:")
+a = int(x)
+y = input("Enter a second number:")
+b = int(y)
+print(a + b)
---
+++ userdivision.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a first number:"))
+y = int(input("Enter a second number:"))
+z = x / y
+print(z)
+
+x = int(input("Enter a first number:"))
+y = int(input("Enter a second number:"))
+print(x / y)
