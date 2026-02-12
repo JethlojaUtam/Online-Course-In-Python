a0f648a1d2c2b88e3b1689c3abbaf5e53582f3e6
Jethloja Utam <utamjethloja@gmail.com>
2026-02-12 18:37:36 +0530

    Add files via upload

---
+++ userfunction.py
@@ -0,0 +1,13 @@
+x = input("Enter a name:")
+print("Hello Friends!!")
+
+x = input("Enter a name:")
+y = input("Enter a name:")
+print("Hello Friends!!")
+
+print("Enter a name:")
+x = input()
+print("Enter a name:")
+y = input()
+print(x,y)
+print(x,"Hello,",y)
---
+++ userfunction1.py
@@ -0,0 +1,9 @@
+x = input("Enter a name:")
+y = input("Enter a name:")
+print(x,y)
+print(x,y,sep=" ")
+
+x = input("Enter a first number:")
+y = input("Enter a second number:")
+print(x,y)
+print(x,y,sep="/")
