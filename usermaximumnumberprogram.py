fb11b8e47087a2367283917463dd847e6452767b
Jethloja Utam <utamjethloja@gmail.com>
2026-02-14 17:45:04 +0530

    Add files via upload

---
+++ usermaximumnumberprogram.py
@@ -0,0 +1,8 @@
+p = int(input("Enter a first number:"))
+q = int(input("Enter a second number:"))
+if p > q:
+	print(p,"is maximum number")
+elif q > p:
+	print(q,"is maximum number")
+else:
+	print("Both are equal number")
---
+++ userminimumnumberprogram.py
@@ -0,0 +1,8 @@
+p = int(input("Enter a first number:"))
+q = int(input("Enter a second number:"))
+if p < q:
+	print(p,"is minimum number")
+elif q < p:
+	print(q,"is minimum number")
+else:
+	print("Both are equal number")
